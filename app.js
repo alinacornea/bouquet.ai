e5d87812d69464298cb8bc6a047eae6bfc580a77
alinacornea <alinacornea18@gmail.com>
2017-09-28 01:55:01 -0700

    whish could do more

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var json = require('./test.json');
+
+const d3 = require('d3node-barchart');
+const output = require('d3node-output');
+
 
 /* routing app.METHOD(PATH, HANDLER);
 * app-instance of express;
@@ -8,12 +13,20 @@ var path = require('path');
 * PATH is a path on the server.
 * HANDLER is the function executed when the route is matched.
 */
-
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname +'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
+console.log(json);
+$.ajax({
+    type: "POST",
+    url: path.join(__dirname +'/index.html'),
+    data: json,
+    success: function(){},
+    dataType: "json",
+    contentType : "application/json"
+  });
 /*
 * start the server
 *
